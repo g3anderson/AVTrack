a949c3406fcd630d9b9a35706a029f3edd2387d5
g3anderson <gfernando3.a@gmail.com>
2025-02-28 11:35:40 -0300

    Nova classe 'Turma'

--- projetoAVTrack/src/main/java/Classes/Turma.java
+++ projetoAVTrack/src/main/java/Classes/Turma.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Classes;
 
-/**
- *
- * @author Dohms
- */
 public class Turma {
     
+    private String nomeSala;
+    private String predioLocalizado; 
+    private int andarSala;
+    
+    public Turma(String nomeSala, String predioLocalizado, int andarSala){
+    
+        this.nomeSala = nomeSala;
+        this.predioLocalizado = predioLocalizado;
+        this.andarSala = andarSala;
+    }
+    
+    public String getNomeSala(){
+        return this.nomeSala;
+    }
+    
+    public void setNomeSala(String novoNomeSala){
+        this.nomeSala =novoNomeSala;
+    }
+    
+    public String getPredioLocalizado(){
+        return this.predioLocalizado;
+    }
+    
+    public void setPredioLocalizado(String novoPredioLocalizado){
+        this.predioLocalizado = novoPredioLocalizado;
+    }
+    
+    public int getAndarSala(){
+        return this.andarSala;
+    }
+    
+    public void setAndarSala(int novoAndarSala){
+        this.andarSala = novoAndarSala;
+    }
 }
